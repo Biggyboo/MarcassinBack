2212cbc633ec726ed45d8ffaed1c4a936c799883
Gautier Julien <jululien.gautier@laposte.net>
2018-06-18 14:24:46 +0200

    ajout du bouton pupitre, modif comp mere

--- MarcassinLightBLL/Appel_List.cs
+++ MarcassinLightBLL/Appel_List.cs
@@ -17,6 +17,37 @@ namespace MarcassinLightBLL
             return listCompBO;
 
         }
+        public static List<Competence_BO> GetCompetencesActives()
+        {
+            
+            List<Competence_BO> listCompBO = Competence_DAL.GetCompetence();
+            List<Competence_BO> listCompBOAct = new List<Competence_BO>();
+            foreach (Competence_BO comp in listCompBO)
+            {
+                if (comp.Est_active==true)
+                {
+                    listCompBOAct.Add(comp);
+                }
+            }
+            return listCompBOAct;
+
+        }
+
+        public static List<Competence_BO> GetCompetencesArchives()
+        {
+
+            List<Competence_BO> listCompBO = Competence_DAL.GetCompetence();
+            List<Competence_BO> listCompBOArc = new List<Competence_BO>();
+            foreach (Competence_BO comp in listCompBO)
+            {
+                if (comp.Est_active == false)
+                {
+                    listCompBOArc.Add(comp);
+                }
+            }
+            return listCompBOArc;
+
+        }
 
         public static List<Categorie_BO> GetCategories()
         {
@@ -41,6 +72,7 @@ namespace MarcassinLightBLL
             return listBadgBO;
 
         }
+<<<<<<< HEAD
         public static List<LangueCategorie_BO> GetLanguesCategoriesByIdCat(Categorie_BO categorie)
         {
             List<LangueCategorie_BO> listLangCat = LangueCategorie_DAL.GetLangueCategoriesByIdCategorie(categorie);
@@ -52,5 +84,8 @@ namespace MarcassinLightBLL
             List<Langue_Badge_BO> listLangBadBO = Langue_Badge_DAL.GetTraductionByBadge(bad);
             return listLangBadBO;
         }
+=======
+
+>>>>>>> ajout du bouton pupitre, modif comp mere
     }
 }
